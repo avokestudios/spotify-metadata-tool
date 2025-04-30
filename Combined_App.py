22e3a74d19862c8814ecfcae3051b757c22258f2
avokestudios <officialavokestudios@gmail.com>
2025-04-30 22:25:40 +0100

    Update Combined_App.py

--- Combined_App.py
+++ Combined_App.py
@@ -26,8 +26,8 @@ from wordcloud import WordCloud
 uploaded_files = []
 
 # ----------------------- METADATA APP FUNCTIONS ----------------------- #
-CLIENT_ID = "206d3b06da384ae480d33a632e22a2a8" ## 'your-client-id'
-CLIENT_SECRET = ##'Your client secret'
+CLIENT_ID = ## 'Your Client ID'
+CLIENT_SECRET = ##'Your Client Secret' (Read me explains how to generate your own)
 
 def authenticate_spotify():
     client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
